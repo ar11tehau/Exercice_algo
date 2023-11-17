8bca4515c4679f38502f80b82aa64871d069e1bf
Arii D <108074652+ar11tehau@users.noreply.github.com>
2023-11-17 14:26:17 +0100

    1 bank fini

--- xv/poo/bank.py
+++ xv/poo/bank.py
@@ -14,10 +14,13 @@ class CompteSimple:
     '''
     CompteSimple avec un titulaire et un solde
     '''
+    numero_primaire = 10000
 
     def __init__(self, titulaire: Personne, solde:int = 0):
         self.__solde = solde
         self.titulaire = titulaire
+        CompteSimple.numero_primaire += 1
+        self.numero = CompteSimple.numero_primaire
     
     @property
     def solde(self):
@@ -28,12 +31,32 @@ class CompteSimple:
     
     def debiter(self, somme: int):
         self.__solde -= somme
-    
+
     def __str__(self):
         return f"Le solde de {self.titulaire} est de : {self.solde}"
 
+
 class CompteCourant(CompteSimple):
-    pass
+    def __init__(self, titulaire: Personne, solde:int = 0):
+        super().__init__(titulaire, solde)
+        self.__list_releve = list()
+    
+    def crediter(self, somme: int):
+        super().crediter(somme)
+        self.__list_releve.append(f"Somme crédité : +{somme}")
+    
+    def debiter(self, somme: int):
+        super().debiter(somme)
+        self.__list_releve.append(f"Somme debité : -{somme}")
+    
+    def afficher_releve(self):
+        if len(self.__list_releve) > 0:
+            print(f"Relevé du compte {self.numero}:")
+            for operation in self.__list_releve:
+                print(4*"-" + ">",operation)
+            print()
+        else:
+            print(f"Relevé du compte {self.numero}: Pas de relevé")
 
 class Banque:
     '''
@@ -46,7 +69,11 @@ class Banque:
     def ouvrir_compte(self, client: Personne, solde: int):
         compte = CompteSimple(client, solde)
         self.__list_compte.append(compte)
-        return compte
+        self.client_exist(client)
+    
+    def ouvrir_compte_courant(self, client: Personne, solde: int):
+        compte = CompteCourant(client, solde)
+        self.__list_compte.append(compte)
     
     @property
     def list_compte(self):
@@ -61,4 +88,12 @@ class Banque:
     def frais(self, somme: int):
         for compte in self.__list_compte:
             compte.debiter(somme)
+    
+    def afficher_releve(self):
+        for compte in self.__list_compte:
+            try:
+                compte.afficher_releve()
+            except AttributeError:
+                pass
+
 
