ac2e955dbae4deeb2b85d74b28dd5442bed647fc
Arii D <108074652+ar11tehau@users.noreply.github.com>
2023-11-17 14:38:51 +0100

    cleaning

--- xv/poo/test_bank.py
+++ xv/poo/test_bank.py
@@ -1,5 +1,6 @@
 from bank import *
 import pytest
+from unittest.mock import patch, call # non trouvé : pip install unittest
 
 @pytest.fixture 
 def p1():
@@ -11,13 +12,24 @@ def p2():
 
 @pytest.fixture 
 def c1():
-    return CompteSimple( p1,25)
+    return CompteSimple(p1, 25)
+
+@pytest.fixture
+def cc1():
+    return CompteCourant(p1, 25)
 
 @pytest.fixture
 def b1():
     b = Banque()
     b.ouvrir_compte(p1, 25)
     b.ouvrir_compte(p1, 75)
+    return b
+
+@pytest.fixture
+def b2():
+    b = Banque()
+    b.ouvrir_compte_courant(p1, 25)
+    b.ouvrir_compte_courant(p1, 75)
     return b
 
 def test_crediter(c1):
@@ -43,5 +55,26 @@ def test_frais(b1):
     assert b1.list_compte[0].solde == 15
     assert b1.calculer_total() == 80
 
-# def test_longueur(b1):
-#     assert len(b1) == 2
+@patch('builtins.print')
+def test_comptecourant(mockprint, cc1):
+    cc1.crediter(25)
+    assert cc1.solde == 50
+    cc1.debiter(25)
+    assert cc1.solde == 25
+    cc1.afficher_releve()
+    assert mockprint.mock_calls == [ call("Un entier est attendu.") ]
+
+
+
+# def test_editer_relever_compte_courant(capfd,c2):
+#     c2.crediter(15)
+#     c2.debiter(25)
+#     c2.editerreleve()
+#     out,  = capfd.readouterr()
+#     assert out == "Credit de 15 euros\nDebit de -25 euros\n"
+
+def test_banque_cc(b2):
+    b2.frais(10)
+    assert b2.list_compte[0].solde == 15
+    assert b2.calculer_total() == 80
+
